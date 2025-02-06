18cff308d0b25723e1df57a5fbca385880107a06
FReptar0 <20213tn137@utez.edu.mx>
2025-02-06 11:50:58 -0600

    Mejorar el comportamiento del encabezado y la estructura de la página de inicio

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,6 +21,8 @@ const Header = () => {
   const [activePath, setActivePath] = useState("");
   const [anchorEl, setAnchorEl] = useState(null); // Control del menú de usuario
   const openMenu = Boolean(anchorEl); // Determina si el menú está abierto
+  const [showMenu, setShowMenu] = useState(true);
+  const [isTop, setIsTop] = useState(true);
 
   const theme = useTheme();
   const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
@@ -62,69 +64,93 @@ const Header = () => {
     handleCloseMenu(); // Cierra el menú al hacer clic en cerrar sesión
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      setIsTop(scrollTop === 0);
+      setShowMenu(scrollTop === 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <AppBar position="static" sx={{ backgroundColor: "#F5F5F5", color: "#000" }}>
-        <Toolbar sx={{ justifyContent: "space-between" }}>
+      <AppBar position="sticky" sx={{ background: isTop ? "#F5F5F5" : "linear-gradient(to bottom, rgba(245, 245, 245, 0.9), rgba(245, 245, 245, 0.7))", color: "#000", transition: "background 0.3s, height 0.3s", height: isTop ? "100px" : "60px" }}>
+        <Toolbar sx={{ justifyContent: "space-between", minHeight: "60px !important" }}>
           {/* LOGO */}
-          <Box sx={{ display: "flex", alignItems: "center", height: "20px" }}>
+          <Box
+            sx={{ display: "flex", alignItems: "center", height: "100%", cursor: "pointer" }}
+            onClick={() => handleNavigation("/")}
+          >
             <img
               src="/logos/Qardeal_logo completo verde y rojo.png"
               alt="Logo"
-              style={{ width: 200, height: "auto" }} // Ajusta el tamaño aquí
+              style={{ width: isTop ? 350 : 200, height: "auto", transition: "width 0.3s" }} // Ajusta el tamaño aquí
             />
           </Box>
 
           {/* MENÚ NORMAL (SOLO EN ESCRITORIO) */}
-          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 5, width: "100%", justifyContent: "end", marginRight: "32px" }}>
-            {["/", "/disponible", "/categorias", "/ofertas"].map((path, index) => (
-              <Button
-                key={index}
-                sx={{
-                  color: "#000",
-                  borderBottom: activePath === path ? "2px solid #141414" : "none",
-                  borderRadius: 0,
-                }}
-                onClick={() => handleNavigation(path)}
-              >
-                {path === "/" ? "Inicio" : path.replace("/", "").charAt(0).toUpperCase() + path.slice(2)}
-              </Button>
-            ))}
-          </Box>
+          {isTop && (
+            <Box sx={{ display: { xs: "none", md: "flex" }, gap: 5, width: "100%", justifyContent: "end", marginRight: "32px" }}>
+              {["/", "/disponible", "/categorias", "/ofertas"].map((path, index) => (
+                <Button
+                  key={index}
+                  sx={{
+                    color: "#000",
+                    textDecoration: "none",
+                    fontWeight: activePath === path ? "bold" : "normal",
+                    borderBottom: activePath === path ? "2px solid #141414" : "none",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleNavigation(path)}
+                >
+                  {path === "/" ? "Inicio" : path.replace("/", "").charAt(0).toUpperCase() + path.slice(2)}
+                </Button>
+              ))}
+            </Box>
+          )}
 
           {/* ICONOS Y MENÚ HAMBURGUESA */}
-          <Box sx={{ display: "flex", gap: 2 }}>
-            <IconButton color="inherit" onClick={handleClickUserIcon}>
-              <AccountCircleIcon />
-            </IconButton>
+          {isTop && (
+            <Box sx={{ display: "flex", gap: 2 }}>
+              <IconButton color="inherit" onClick={handleClickUserIcon}>
+                <AccountCircleIcon />
+              </IconButton>
 
-            {/* MENÚ DESPLEGABLE DE USUARIO */}
-            <Menu
-              anchorEl={anchorEl} // El elemento que dispara el menú
-              open={openMenu} // Si el menú está abierto
-              onClose={handleCloseMenu} // Cerrar el menú al hacer clic fuera
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "right",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-            >
-              <MenuItem onClick={handleCloseMenu}>Mi perfil</MenuItem>
-              <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
-            </Menu>
+              {/* MENÚ DESPLEGABLE DE USUARIO */}
+              <Menu
+                anchorEl={anchorEl} // El elemento que dispara el menú
+                open={openMenu} // Si el menú está abierto
+                onClose={handleCloseMenu} // Cerrar el menú al hacer clic fuera
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+              >
+                <MenuItem onClick={
+                  () => handleNavigation("/perfil")
+                }>Mi perfil</MenuItem>
+                <MenuItem onClick={handleLogout}>Cerrar sesión</MenuItem>
+              </Menu>
 
-            {/* ÍCONO MENÚ (SOLO EN MÓVILES) */}
-            <IconButton
-              color="inherit"
-              sx={{ display: { md: "none" } }}
-              onClick={() => setOpen(!open)}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Box>
+              {/* ÍCONO MENÚ (SOLO EN MÓVILES) */}
+              <IconButton
+                color="inherit"
+                sx={{ display: { md: "none" } }}
+                onClick={() => setOpen(!open)}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Box>
+          )}
         </Toolbar>
       </AppBar>
 
@@ -132,15 +158,14 @@ const Header = () => {
       <Collapse in={open} timeout="auto" unmountOnExit>
         <Box sx={{ backgroundColor: "#F5F5F5", textAlign: "center", py: 2, paddingTop: "0px" }}>
           {["/", "/disponible", "/categorias", "/ofertas"].map((path, index) => (
-            <Box sx={{ display: "flex", width: "100%", justifyContent: "center" }} id="xd">
+            <Box key={index} sx={{ display: "flex", width: "100%", justifyContent: "center" }}>
               <Link
-                key={index}
+                href={path}
                 sx={{
                   color: "#000",
                   display: "block",
                   textDecoration: "none",
                   width: "90%",
-                  display: "flex",
                   py: 1,
                   fontFamily: `"Roboto", "Helvetica", "Arial", sans-serif`,
                   fontWeight: 500,
