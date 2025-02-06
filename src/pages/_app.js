e0c2b9b22e530b393893cc78995d7b29d0b808fc
FReptar0 <20213tn137@utez.edu.mx>
2025-02-05 21:49:45 -0600

    Agregar tema oscuro y eliminar archivos SVG no utilizados

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import darkTheme from '../utils/theme';
+import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+                <link href="/favicon.ico" rel="shortcut icon" type="image/x-icon"></link>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <ThemeProvider theme={darkTheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
+
+export default MyApp;
