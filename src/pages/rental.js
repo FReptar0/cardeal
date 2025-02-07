cbe52f97a85e84324d01183fa59af957bb0798ad
FReptar0 <20213tn137@utez.edu.mx>
2025-02-06 18:29:49 -0600

    REFAC: Eliminar comentarios innecesarios en la página de alquiler

--- src/pages/rental.js
+++ src/pages/rental.js
@@ -16,12 +16,10 @@ export default function RentalPage() {
     return (
         <Container sx={{ marginTop: 4, maxWidth: "100vw", paddingX: 2 }}>
             <Box sx={{ display: "flex", flexDirection: { xs: "column", md: "row" }, minHeight: "85vh", alignItems: "flex-start", width: "100%" }}>
-                {/* LADO IZQUIERDO - BUSCADOR */}
                 <Box sx={{ width: { xs: "100%", md: "30%" }, paddingRight: { md: 2 }, marginBottom: { xs: 2, md: 0 }, display: "flex", justifyContent: "center" }}>
                     <SearchBar onSearch={handleSearch} />
                 </Box>
 
-                {/* LADO DERECHO - TARJETAS DE AUTOS */}
                 <Box sx={{ pb: 3, width: { xs: "100%", md: "70%" }, paddingLeft: { md: 2 }, display: "flex", flexDirection: "column", gap: 2, minHeight: "80vh", overflowY: "auto" }}>
                     {filteredCars.map((car, index) => (
                         <CarCard key={index} car={car} />
