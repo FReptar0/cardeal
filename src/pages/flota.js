114fb53aeb41b1615e9655a4fa26f364a232cc4a
FReptar0 <20213tn137@utez.edu.mx>
2025-02-06 20:49:28 -0600

    NUEVO: Agregar barra de búsqueda en la página Flota y ajustar estilos en el componente Index

--- src/pages/flota.js
+++ src/pages/flota.js
@@ -2,14 +2,110 @@ import React, { useState, useEffect } from "react";
 import { Box, Card, CardMedia, CardContent, Typography, Grid } from "@mui/material";
 import LuggageIcon from "@mui/icons-material/Luggage";
 import carData from "../data/cars.json";
+import SearchBar from "../components/SearchBar";
 
 export default function Flota() {
+   const handleSearch = (filters) => {
+      setFilteredCars(carsData);
+    };
   return (
     <Box sx={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", p: 4, marginTop: "120px" }}>
-    <Box ><Typography variant="h4" gutterBottom textAlign="center" color="black" >
+      <div
+        id="sectionOne"
+        style={{
+          height: "110vh",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+          {/* Sección Izquierda */}
+          <Grid item xs={12} md={12} lg={6} sx={{
+            display: { xs: "none", lg: "block" } // Solo mostrar en pantallas grandes
+          }} style={{ position: "relative" }}>
+            {/* Fondo detrás de la imagen */}
+            {/* Imagen de fondo */}
+            <img
+              src="/extras/background-mexico-light.png"
+              alt="Fondo"
+              style={{
+                width: "600px", // Ajusta el tamaño del fondo
+                height: "auto",
+                position: "absolute",
+                top: "10%",
+                left: "52%",
+                transform: "translate(-50%, -50%)",
+                zIndex: -1, // Envía el fondo detrás de la imagen principal
+              }}
+            />
+
+            {/* Imagen en primer plano */}
+            <img
+              style={{
+                width: "80vh",
+                position: "relative",
+                top: "30px",
+                zIndex: 0,
+              }}
+              src="/cars/CRV.png"
+              alt="Auto"
+            />
+          </Grid>
+          {/* Sección Derecha */}
+          <Grid item xs={12} md={12} lg={6} sx={{ zIndex: 2 }}>
+            <div
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div style={{ position: "absolute", top: "100px", width: "500px" }}>
+                <h2
+                  id='sectionDerechaTitle'
+                  style={{
+                    position: "relative",
+                    top: "40px",
+                    fontWeight: 300,
+                    fontSize: "2rem",
+                    textAlign: "start",
+                    marginBottom: "24px",
+                  }}
+                >
+                  <span style={{ color: "#ff3f60", fontWeight: 900 }}>
+                    Renta un sueño, renta un auto
+                  </span>{" "}
+                  <br />{" "}
+                  <span
+                    style={{
+                      fontSize: "1.3rem",
+                      fontWeight: "600",
+                      position: "relative",
+                      top: "-10px",
+                    }}
+                  >
+                    {" "}
+                    Rápido y sencillo{" "}
+                  </span>
+                </h2>
+              </div>
+              <div
+                className="custom-search-bar"
+                style={{ width: "40%", position: "absolute", top: "250px" }}
+              >
+                <SearchBar onSearch={handleSearch} />
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+      <Box sx={{
+        marginTop: -35,
+      }}><Typography variant="h4" gutterBottom textAlign="center" color="black" >
         FLOTA DE AUTOS
       </Typography></Box>
-      
+
       <Typography variant="body1" gutterBottom textAlign="center" maxWidth="600px" color="black">
         Queremos que tu viaje sea cómodo y seguro, ofreciendo una amplia categoría de vehículos.
       </Typography>
@@ -18,10 +114,10 @@ export default function Flota() {
           <Grid item xs={12} sm={6} md={3} key={index}>
             <Card sx={{ display: "flex", flexDirection: "column", height: "100%", backgroundColor: "white", color: "black" }}>
               <Box sx={{ width: "100%", height: 150, display: "flex", justifyContent: "center", alignItems: "center", overflow: "hidden" }}>
-                <CardMedia 
-                  component="img" 
-                  image={car.image} 
-                  alt={car.name} 
+                <CardMedia
+                  component="img"
+                  image={car.image}
+                  alt={car.name}
                   sx={{ width: "100%", height: "100%", objectFit: "contain" }}
                 />
               </Box>
