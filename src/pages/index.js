fea6ef74b9b650fa773d230ea06aa8b105578225
Yahir Alberto <136031489+yahirdev13@users.noreply.github.com>
2025-02-06 20:32:47 -0600

    REFAC: Cambiar posición de div en el componente Index de 'fixed' a 'absolute' y ajustar ancho

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ export default function Index() {
                 flexDirection: "column",
               }}
             >
-              <div style={{ position: "fixed", top: "100px", width: "100%" }}>
+              <div style={{ position: "absolute", top: "100px", width: "450px" }}>
                 <h2
                   id='sectionDerechaTitle'
                   style={{
